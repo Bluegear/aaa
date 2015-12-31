536f7b96035ebe6c67afabd6e75b857a727fc706
Chatri Tung <gearmag@gmail.com>
2015-12-31 11:56:09 +0700

    Add model and migration to mcprofile.

--- mcprofile/models.py
+++ mcprofile/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Merchant(models.Model):
+    display_name = models.CharField(max_length=100)
+
